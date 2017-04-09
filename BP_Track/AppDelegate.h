39830e3d19581f2b23490b5e37635da30b77f4c4
Akshat <akshat@akshats-macbook-pro.local>
2017-04-09 18:50:05 -0400

    Initial Commit

---
+++ BP_Track/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BP_Track
+//
+//  Created by Akshat on 09/04/17.
+//  Copyright © 2017 Akshat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BP_Track/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BP_Track
+//
+//  Created by Akshat on 09/04/17.
+//  Copyright © 2017 Akshat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
